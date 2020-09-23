dd78134b4411e0c61f16a462430d951fcf9187b8
Italo Nunes <italospb7@gmail.com>
2020-09-23 13:24:40 -0300

    aws

--- i-one/backend/src/config/upload.ts
+++ i-one/backend/src/config/upload.ts
@@ -1,19 +1,42 @@
 import path from 'path';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
 
 const tempFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tempFolder: string;
+  uploadFolder: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 export default {
+  driver: process.env.STORAGE_DRIVER,
   tempFolder,
   uploadFolder: path.resolve(tempFolder, 'uploads'),
+  multer: {
+    storage: multer.diskStorage({
+      destination: tempFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-  storage: multer.diskStorage({
-    destination: tempFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHash}-${file.originalname}`;
-
-      return callback(null, fileName);
+        return callback(null, fileName);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-idealizesoft',
     },
-  }),
-};
+  },
+} as IUploadConfig;
