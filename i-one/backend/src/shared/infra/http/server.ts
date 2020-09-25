b0af392ef0d7e484845cd4512678a7029facbda5
Italo Nunes <italospb7@gmail.com>
2020-09-25 17:45:50 -0300

    server

--- i-one/backend/src/shared/infra/http/server.ts
+++ i-one/backend/src/shared/infra/http/server.ts
@@ -30,11 +30,13 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     });
   }
 
+  console.log(err);
+
   return response.status(500).json({
     status: 'error',
     message: 'Internal server error',
   });
 });
 app.listen(3333, () => {
-  console.log('Server started on port 3333');
+  console.log('🚀 Server started on port 3333');
 });
