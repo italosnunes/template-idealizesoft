5ff844ad0c6fc0a067e1104de83d13767616e257
Italo Nunes <italospb7@gmail.com>
2020-09-29 10:35:57 -0300

    providers-list

--- i-one/backend/src/modules/appointments/services/ListProvidersService.ts
+++ i-one/backend/src/modules/appointments/services/ListProvidersService.ts
@@ -4,6 +4,7 @@ import { injectable, inject } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUserRepository from '@modules/users/repositories/IUserRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -29,7 +30,10 @@ export default class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
